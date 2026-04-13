e2120ddc830a143e38479a4b24d65f37c0e648d2
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2026-04-13 11:22:46 +0000

    feat: create not found page

---
+++ website/src/routes/not-found.tsx
@@ -0,0 +1,22 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { Footer } from "@/components/website/footer";
+import { Navbar } from "@/components/website/navbar";
+import { NotFoundHero } from "@/components/website/not-found-hero";
+import { LocationsFooter } from "@/components/website/not-found-locations-footer";
+
+export const Route = createFileRoute("/not-found")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return (
+    <div className="min-h-screen bg-black font-sans">
+      <Navbar />
+      <main>
+        <NotFoundHero />
+      </main>
+      <LocationsFooter />
+      <Footer />
+    </div>
+  );
+}
