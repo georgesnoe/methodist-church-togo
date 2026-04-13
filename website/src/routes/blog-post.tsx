374b394a51054a6daf0b9cb5ebf28b34c30e9afb
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2026-04-13 11:23:29 +0000

    feat: create blog post page

---
+++ website/src/routes/blog-post.tsx
@@ -0,0 +1,26 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { ArticleContent } from "@/components/website/article-content";
+import { BlogPostHero } from "@/components/website/blog-post-hero";
+import { LocationsFooter } from "@/components/website/blog-post-locations-footer";
+import { Footer } from "@/components/website/footer";
+import { LatestPosts } from "@/components/website/latest-posts";
+import { Navbar } from "@/components/website/navbar";
+
+export const Route = createFileRoute("/blog-post")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return (
+    <div className="min-h-screen bg-white font-sans">
+      <Navbar />
+      <main>
+        <BlogPostHero />
+        <ArticleContent />
+        <LatestPosts />
+      </main>
+      <LocationsFooter />
+      <Footer />
+    </div>
+  );
+}
